f97c043a23fb7c04aefac63bd15f2f6e1f2df836
music-downloader <wazihsabir@gmail.com>
2017-06-17 22:38:14 -0500

    Added bootstrap and styling

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -40,9 +40,9 @@ function  getOutput(item) {
     var videoDate = new Date(item.snippet.publishedAt).toISOString().slice(0, 10);;
 
     // Build output string
-    var output = '<li style="list-style: none; border-top: 2px dashed; border-bottom: 2px dashed;">' +
+    var output = '<li style="list-style: none; border-bottom: 2px dashed;">' +
         '<div class="list-right" style="padding-bottom: 20px;">' +
-        '<h5><a href="https://www.youtube.com/watch?v='+videoId+' " onclick="getLinks(event)">'+title+'</a></h5>' +
+        '<h5><a href="https://www.youtube.com/watch?v='+videoId+'" onclick="getLinks(event)">'+title+'</a></h5>' +
         '<small>By <span class="cTitle" style="color: rgba(171,21,0,0.98);">'+channelTitle+'' +
         '</span> on '+videoDate+'</small>' +
         '</div>' +
@@ -55,9 +55,14 @@ function  getOutput(item) {
 function getLinks(event) {
     event.preventDefault();
     var link = $(event.target).attr("href");
-    var list_link = '<li style="list-style: none;">' +
-            '<iframe style="width:230px;height:60px;border:0;overflow:hidden;" scrolling="no" src="//www.youtubeinmp3.com/widget/button/?video='+link+'"&color=c91818></iframe>' +
+    var list_link = '<li style="list-style: none; display: inline; padding-right: 10px;">' +
+            '<iframe style="width:230px;height:60px;border:0;overflow:hidden;" scrolling="no" src="//www.youtubeinmp3.com/widget/button/?video='+link+'"&color=96c864></iframe>' +
             '</li>' +
             '';
-    $('#selected').append(list_link)
+    // if ($('#selected li').size() > 3) {
+    //     $('#selected').empty();
+    //     $('#selected').append(list_link);
+    // }else {
+    // }
+    $('#selected').append(list_link);
 }
